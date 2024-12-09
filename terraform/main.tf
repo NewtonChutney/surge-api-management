c921a1a4455bfb14f21090ec9f1e7cb620c70d49
NewtonChutney <70827815+NewtonChutney@users.noreply.github.com>
2024-12-09 14:50:36 +0530

    refactor deployment

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,64 +43,64 @@ resource "azurerm_linux_function_app" "function_app" {
   }
 }
 
-# resource "azurerm_api_management" "apim" {
-#   name                = "rust-trigger-api-mngmt"
-#   location            = azurerm_resource_group.rg.location
-#   resource_group_name = azurerm_resource_group.rg.name
-#   publisher_email     = "newtonchutney@example.com"
-#   publisher_name      = "API Admin"
-#   sku_name            = "Developer_1"
-# }
+resource "azurerm_api_management" "apim" {
+  name                = "rust-trigger-api-mngmt"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  publisher_email     = "newtonchutney@example.com"
+  publisher_name      = "API Admin"
+  sku_name            = "Developer_1"
+}
 
-# resource "azurerm_api_management_api" "api" {
-#   name                = "AzureApimAPI"
-#   resource_group_name = azurerm_resource_group.rg.name
-#   api_management_name = azurerm_api_management.apim.name
-#   revision            = "1"
-#   display_name        = "Rust Trigger API"
-#   path                = "rust-trigger-mgnmt"
-#   protocols           = ["https"]
-#   service_url         = "https://${azurerm_linux_function_app.function_app.default_hostname}"
-# }
+resource "azurerm_api_management_api" "api" {
+  name                = "AzureApimAPI"
+  resource_group_name = azurerm_resource_group.rg.name
+  api_management_name = azurerm_api_management.apim.name
+  revision            = "1"
+  display_name        = "Rust Trigger API"
+  path                = "rust-trigger-mgnmt"
+  protocols           = ["https"]
+  service_url         = "https://${azurerm_linux_function_app.function_app.default_hostname}"
+}
 
-# # APIM Security: Add API Key Policy
-# resource "azurerm_api_management_api_operation" "api_operation" {
-#   operation_id        = "get-default"
-#   api_name            = azurerm_api_management_api.api.name
-#   api_management_name = azurerm_api_management.apim.name
-#   resource_group_name = azurerm_resource_group.rg.name
-#   display_name        = "Default"
-#   method              = "GET"
-#   url_template        = "/"
-# }
+# APIM Security: Add API Key Policy
+resource "azurerm_api_management_api_operation" "api_operation" {
+  operation_id        = "get-default"
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
+  display_name        = "Default"
+  method              = "GET"
+  url_template        = "/"
+}
 
-# resource "azurerm_api_management_api_policy" "api_policy" {
-#   api_name            = azurerm_api_management_api.api.name
-#   api_management_name = azurerm_api_management.apim.name
-#   resource_group_name = azurerm_resource_group.rg.name
+resource "azurerm_api_management_api_policy" "api_policy" {
+  api_name            = azurerm_api_management_api.api.name
+  api_management_name = azurerm_api_management.apim.name
+  resource_group_name = azurerm_resource_group.rg.name
 
-#   xml_content = <<EOT
-#     <policies>
-#       <inbound>
-#         <base />
-#         <set-header name="X-Frame-Options" exists-action="override">
-#           <value>Deny</value>
-#         </set-header>
-#         <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized">
-#           <openid-config url="https://login.microsoftonline.com/{tenantId}/v2.0/.well-known/openid-configuration" />
-#           <required-claims>
-#             <claim name="aud">
-#               <value>api://your-api-client-id</value>
-#             </claim>
-#           </required-claims>
-#         </validate-jwt>
-#       </inbound>
-#       <backend>
-#         <base />
-#       </backend>
-#       <outbound>
-#         <base />
-#       </outbound>
-#     </policies>
-#   EOT
-# }
+  xml_content = <<EOT
+    <policies>
+      <inbound>
+        <base />
+        <set-header name="X-Frame-Options" exists-action="override">
+          <value>Deny</value>
+        </set-header>
+        <validate-jwt header-name="Authorization" failed-validation-httpcode="401" failed-validation-error-message="Unauthorized">
+          <openid-config url="https://login.microsoftonline.com/{tenantId}/v2.0/.well-known/openid-configuration" />
+          <required-claims>
+            <claim name="aud">
+              <value>api://your-api-client-id</value>
+            </claim>
+          </required-claims>
+        </validate-jwt>
+      </inbound>
+      <backend>
+        <base />
+      </backend>
+      <outbound>
+        <base />
+      </outbound>
+    </policies>
+  EOT
+}
